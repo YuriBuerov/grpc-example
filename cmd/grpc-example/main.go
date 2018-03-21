c64f2395777d5bef7c4266cd9f822e8c347c709b
Yuri Buerov <yuribuerov@gmail.com>
2018-03-21 15:41:59 +0200

    Catch stop signals

--- cmd/grpc-example/main.go
+++ cmd/grpc-example/main.go
@@ -6,6 +6,10 @@ import (
 
 	"github.com/YuriBuerov/grpc-example/pkg/server"
 	"github.com/go-kit/kit/log"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"os/signal"
+	"syscall"
 )
 
 // We don't have config in this example, so port defined as constant
@@ -27,12 +31,15 @@ func main() {
 	}()
 
 	// Initialize GRPC server
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	grpcServer, err := server.NewGRPCServer(logger)
 	if err != nil {
 		logger.Log("error", "failed to init grpc server", "cause", err)
 		os.Exit(1)
 	}
 	defer grpcServer.GracefulStop()
+	catchStopSignal(ctx.Done(), cancel, grpcServer)
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -45,4 +52,18 @@ func main() {
 		logger.Log("error", "GRPC server", "cause", err)
 		os.Exit(1)
 	}
+}
+
+func catchStopSignal(done <-chan struct{}, cancel context.CancelFunc, server *grpc.Server) {
+	var stop = make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-stop:
+			cancel()
+		case <-done:
+		}
+		server.GracefulStop()
+	}()
 }
